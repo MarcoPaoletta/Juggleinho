1404b52b42ee8c0328de59ea7a2b3864681a4c7f
Marco Paoletta <marcopaoletta2007@gmail.com>
2023-03-12 12:00:24 -0300

    Change zoom system if is a big or small device

--- Assets/Scripts/Main/MainCamera/FitToField.cs
+++ Assets/Scripts/Main/MainCamera/FitToField.cs
@@ -11,11 +11,16 @@ public class FitToField : MonoBehaviour
         float screenRatio = (float)Screen.width / (float)Screen.height;
         float targetRatio = fieldSpriteRenderer.bounds.size.x / fieldSpriteRenderer.bounds.size.y;
 
-        if (screenRatio >= targetRatio){
+        if(screenRatio >= targetRatio) // big devices (tablets and iPads)
+        {
+            print("Big device");
+            Camera.main.backgroundColor = Color.black;
             Camera.main.orthographicSize = fieldSpriteRenderer.bounds.size.y / 2;
         }
-        else
+        else // small devices
         {
+            print("Small device");
+            Camera.main.backgroundColor = fieldSpriteRenderer.color;
             float differenceInSize = targetRatio / screenRatio;
             Camera.main.orthographicSize = fieldSpriteRenderer.bounds.size.y / 2 * differenceInSize;
         }
