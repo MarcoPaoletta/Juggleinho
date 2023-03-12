404a4126775c7dd927e28aec02412807df53abee
Marco Paoletta <marcopaoletta2007@gmail.com>
2023-03-12 20:01:46 -0300

    Change ball impulse force

--- Assets/Scripts/Main/Boot/BootCollisionDetection.cs
+++ Assets/Scripts/Main/Boot/BootCollisionDetection.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class BootCollisionDetection : MonoBehaviour
 {
+    private float impulseForce = 8.5f;
 
-    void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Ball"))
         {
             Rigidbody2D collidingRb = collision.gameObject.GetComponent<Rigidbody2D>();
             Vector2 direction = collision.contacts[0].point - (Vector2)transform.position;
             direction = direction.normalized;
-
-            float impulseForce = 10f;
             collidingRb.AddForce(direction * impulseForce, ForceMode2D.Impulse);
         }
     }   
