ef7fac79c7c2cefefada99071a3eb35a6f51d3ce
Marco Paoletta <marcopaoletta2007@gmail.com>
2023-03-12 11:07:45 -0300

    Created Orthographic camera for perfect zoom

---
+++ Assets/Scripts/Main/MainCamera/FitToField.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FitToField : MonoBehaviour
+{
+    public SpriteRenderer fieldSpriteRenderer;
+
+	private void Start () 
+    {
+        float screenRatio = (float)Screen.width / (float)Screen.height;
+        float targetRatio = fieldSpriteRenderer.bounds.size.x / fieldSpriteRenderer.bounds.size.y;
+
+        if (screenRatio >= targetRatio){
+            Camera.main.orthographicSize = fieldSpriteRenderer.bounds.size.y / 2;
+        }
+        else
+        {
+            float differenceInSize = targetRatio / screenRatio;
+            Camera.main.orthographicSize = fieldSpriteRenderer.bounds.size.y / 2 * differenceInSize;
+        }
+	}
+}
