1dda2324d88c3225e15c0d42527254ba8a8d8d96
Marco Paoletta <marcopaoletta2007@gmail.com>
2023-03-11 22:59:12 -0300

    Boot rotation

--- Assets/Scripts/Main/Boot/BootMovement.cs
+++ Assets/Scripts/Main/Boot/BootMovement.cs
@@ -56,12 +56,22 @@ public class BootMovement : MonoBehaviour
             touchPosition.z = 10; // Set a z value for the touch position
             Vector3 curPosition = Camera.main.ScreenToWorldPoint(touchPosition) + offset;
             transform.position = curPosition;
+            
+            // Rotate the object based on the touch movement
+            float rotateSpeed = 10f;
+            float rotateAmount = Input.GetTouch(0).deltaPosition.x * rotateSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.forward, rotateAmount, Space.World);
         }
         else if (Input.GetMouseButton(0))
         {
             Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
             Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
             transform.position = curPosition;
+            
+            // Rotate the object based on the mouse movement
+            float rotateSpeed = 10f;
+            float rotateAmount = Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.forward, rotateAmount, Space.World);
         }
     }
 
