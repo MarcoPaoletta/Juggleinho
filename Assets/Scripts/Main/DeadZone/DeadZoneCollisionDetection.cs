a2d05803c8bcde3eb72fc36a0f75c7eaef05b04b
Marco Paoletta <marcopaoletta2007@gmail.com>
2023-03-13 14:33:48 -0300

    Added DeadZone

---
+++ Assets/Scripts/Main/DeadZone/DeadZoneCollisionDetection.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeadZoneCollisionDetection : MonoBehaviour
+{
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Ball"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }      
+    }
+}
