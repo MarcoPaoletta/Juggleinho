98870b38a09b8d0b2e2848fbe33f5bcee1c329d3
Marco Paoletta <marcopaoletta2007@gmail.com>
2023-03-19 21:55:55 -0300

    Deleted player clamp variables

--- Assets/Scripts/Main/Player/PlayerMovement.cs
+++ Assets/Scripts/Main/Player/PlayerMovement.cs
@@ -7,8 +7,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
 
     private float speed = 10f;
-    private float minPositionX = -2.25f;
-    private float maxPositionX = 2.25f;
 
     private void Update()
     {
