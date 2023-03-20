32865e8bcb2485656f034fd14deef7beb4cfd649
Marco Paoletta <marcopaoletta2007@gmail.com>
2023-03-19 21:14:13 -0300

    Changed player system
    Now the player is just a square in the bottom of the screen moving along the X axis

---
+++ Assets/Scripts/Main/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+
+    private float speed = 10f;
+    private float minPositionX = -2.25f;
+    private float maxPositionX = 2.25f;
+
+    private void Update()
+    {
+        Move();
+        ClampPosition();
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, rb.velocity.y);
+    }
+
+    private void ClampPosition()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minPositionX, maxPositionX), transform.position.y, transform.position.z);
+    }
+}
